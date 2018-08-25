9b11cc4074eb781c0a85c5f60ada5b43ad674ead
vishnu3 <jethliyavishnu@gmail.com>
2018-08-26 00:35:23 +0530

    test branch

--- src/main/java/com/analytics/stockprice/StockpriceApplication.java
+++ src/main/java/com/analytics/stockprice/StockpriceApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class StockpriceApplication {
 
 	public static void main(String[] args) {
+		System.out.println("First git commit");
 		SpringApplication.run(StockpriceApplication.class, args);
 	}
 }
