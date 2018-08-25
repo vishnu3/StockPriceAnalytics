3530fc2e8e64a2efeded14f702d1823e46abbd77
vishnu3 <jethliyavishnu@gmail.com>
2018-08-26 00:29:20 +0530

    Added project to GIT

---
+++ src/main/java/com/analytics/stockprice/StockpriceApplication.java
@@ -0,0 +1,12 @@
+package com.analytics.stockprice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockpriceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockpriceApplication.class, args);
+	}
+}
